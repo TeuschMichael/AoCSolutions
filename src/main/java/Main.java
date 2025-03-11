71139d4953a9dbfa439032d83399cace56dd77d6
TeuschMichael <127761578+TeuschMichael@users.noreply.github.com>
2025-03-11 13:23:38 +0100

    update for branch: removed if statement and used ternary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,19 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.nio.file.Files.find;
-import static java.nio.file.Files.readAllLines;
+import java.nio.file.*;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        File file = new File("./AoC1.txt");
-        Path sourcePath = FileSystems.getDefault().getPath("F:/Code Projects/Java/Advent of Code/AoC1/AoC1.txt");
-        List<String> sourceList = readAllLines(sourcePath);
+
+        List<String> sourceList = Files.readAllLines(Path.of("F:/Code Projects/Java/Advent of Code/AoC1/AoC1.txt"));
         System.out.println(sourceList.size());
 
-        String[] sArr;
+
 
         List<String> listA = new ArrayList<>();
         List<String> listB = new ArrayList<>();
@@ -26,7 +25,7 @@ public class Main {
 
 
         for (String s : sourceList) {
-            sArr = s.split("   ");
+            String[] sArr = s.split("   ");
             System.out.println("sArr0 : "+sArr[0]);
             listA.add(sArr[0]);
             listB.add(sArr[1]);
@@ -43,13 +42,7 @@ public class Main {
         for(int i = 0; i < listA.size(); i++) {
             int a = Integer.parseInt(listA.get(i));
             int b = Integer.parseInt(listB.get(i));
-            int distance = 0;
-                if (a < b) {
-                    distance = b - a;
-                } else {
-                    distance = a - b;
-                }
-            totalDistance = totalDistance + distance;
+            totalDistance +=  (a < b) ? b - a : a - b;
         }
         System.out.println("Total Distance = " + totalDistance);
     }
